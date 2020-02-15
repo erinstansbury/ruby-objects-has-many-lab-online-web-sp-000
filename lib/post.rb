6e5d04609c23d00d08392cf1504553d9102d3f30
Erin Stansbury <ekstansbury@gmail.com>
2020-02-15 23:22:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,18 @@
 class Post
+  attr_accessor :title :artist
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if author
+      self.artist.name
+    else
+      nil
+    end
+  end
 
 end
