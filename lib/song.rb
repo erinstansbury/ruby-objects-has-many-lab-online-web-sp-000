324ffdd7f2d2c9a58603a19d9c9c081d44babb7c
Erin Stansbury <ekstansbury@gmail.com>
2020-02-15 22:07:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song
   attr_accessor :artist, :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self.name
   end
 
   def artist_name
@@ -11,5 +13,9 @@ class Song
     else
       nil
     end
+  end
+
+  def self.all
+    @@all
   end
 end
