b9b05ab284c37684659a96fcda05c0bb98b04882
Erin Stansbury <ekstansbury@gmail.com>
2020-02-15 22:04:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,29 @@
 class Artist
+  attr_accessor :name
+  @@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = [] #empty collection of songs
+  end
 
-    attr_accessor :name, :songs
+  def songs
+    @songs
+  end
 
-    @@song_count = 0
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-    def initialize(name)
-      @name = name
-      @songs = []
-    end
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-    def add_song(song)
-      @songs << song
-      song.artist = self
-      @@song_count += 1
-    end
-
-    def add_song_by_name(name)
-      song = Song.new(name)
-      @songs << song
-      song.artist = self
-      @@song_count += 1
-    end
-
-    def self.song_count
-      @@song_count
-    end
-
+  def self.song_count
+    @@song_count
+  end
 end
