a30c76ec9fe4369660f5b9994cc8f03791492cfc
Erin Stansbury <ekstansbury@gmail.com>
2020-02-15 21:39:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,19 +2,29 @@ class Artist
   attr_accessor :name
 
   def initialize(name)
-    @name = name
-  end
+    attr_accessor :name, :songs
 
-  def add_song(song)
-    song.artist = self
-  end
+    @@song_count = 0
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
 
-  def add_song_by_name(name, artist)
-  song = Song.new(name, artist)
-  add_song(song)
-end
+    def add_song(song)
+      @songs << song
+      song.artist = self
+      @@song_count += 1
+    end
+
+    def add_song_by_name(name)
+      song = Song.new(name)
+      @songs << song
+      song.artist = self
+      @@song_count += 1
+    end
+
+    def self.song_count
+      @@song_count
+    end
 end
