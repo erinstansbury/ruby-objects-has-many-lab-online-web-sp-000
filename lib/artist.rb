b6dc36138e2090a3351b693df40a66a0525f472e
Erin Stansbury <ekstansbury@gmail.com>
2020-02-15 21:37:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,4 +12,9 @@ class Artist
   def songs
     Song.all.select {|song| song.artist == self}
   end
+
+  def add_song_by_name(name, artist)
+  song = Song.new(name, artist)
+  add_song(song)
+end
 end
